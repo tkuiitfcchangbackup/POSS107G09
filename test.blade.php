3850d979f3a69c068a92508e94bd6e290fadd5d8
az987963 <50192416+az987963@users.noreply.github.com>
2019-06-13 00:29:23 +0800

    Create test.blade.php

---
+++ test.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+  <head>
+    <title>TKU</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+	
+	<meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <link rel="shortcut icon" href="TemplateData/favicon.ico">
+    <link rel="stylesheet" href="TemplateData/style.css">
+	
+  </head>
+  <body>
+  
+ <section class="hero-wrap ">
+        <div class="row no-gutters slider-text align-items-baseline justify-content-end">
+          <div class="col-md-12 text-center ftco-animate">
+            <h1 class="mt-5" style="background-image: url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">TKU-test</h1>
+            <h2>Multimedia</h2>
+          </div>
+        </div>
+    </section>
+
+    <section class="ftco-section">
+    	<div class="container">
+    		<div class="row justify-content-center mb-5">
+				
+			<div class="tag-widget post-tag-container mb-5 mt-5">
+              <div class="tagcloud">
+				<a href="{{ url('/') }}" class="tag-cloud-link">Home</a>
+                <a href="{{ url('/image') }}" class="tag-cloud-link">Image</a>
+                <a href="{{ url('/voice') }}" class="tag-cloud-link">Voice</a>
+                <a href="{{ url('/word') }}" class="tag-cloud-link">Word</a>
+                <a href="{{ url('/test') }}" class="tag-cloud-link">Test</a>
+              </div>
+            </div>
+			</div>
+		</div>
+	</section>	 
+	
+	<div class="row justify-content-center mb-5">
+          <div class="col-md-7 heading-section text-center ftco-animate ">
+          	
+            <h2 class="mb-3">Key Words</h2>
+            <p>Multimedia , Watermark , Teleconference , VirtualReality , Subsampling ,
+                                     Scalability , Overlay , Morph , Keyframe , Hyperlink , Filtering , FPX ,
+                                     Digitisation , Decompression , Compression , Applet , Animation , Anamorphic , Algorithm , Acrobat , Authoring , FPS
+</p>
+          </div>
+		 </div>
+
+
+    <footer class="ftco-footer ftco-section img"> 	
+		<div class="row justify-content-center mb-5">
+        <div class="col-md-7 heading-section text-center ftco-animate ">
+          <h2 class="mb-3">TestGame</h2>
+        </div>
+	</div>
+
+	<div class="row justify-content-center mb-5">
+		<iframe src="./testgameweb/index.html" name="Keywords" scrolling="no" frameborder="0" marginheight="px" marginwidth="1280px" height="720px" width="1280px"></iframe>
+	</div>
+    </footer>
+
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+ <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>-->
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
