d77aa1e286df2fb6c4ba32b3264115c4605929aa
az987963 <50192416+az987963@users.noreply.github.com>
2019-06-13 00:29:10 +0800

    Create word.blade.php

---
+++ word.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+  <head>
+    <title>TKU</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+
+	
+  </head>
+  <body>
+  
+    <section class="hero-wrap ">
+        <div class="row no-gutters slider-text align-items-baseline justify-content-end">
+          <div class="col-md-12 text-center ftco-animate">
+            <h1 class="mt-5" style="background-image: url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">TKU-word</h1>
+            <h2>Multimedia</h2>
+          </div>
+        </div>
+    </section>
+
+    	<div class="container">
+    		<div class="row justify-content-center mb-5">
+				
+			<div class="tag-widget post-tag-container mb-5 mt-5">
+              <div class="tagcloud">
+				<a href="{{ url('/') }}" class="tag-cloud-link">Home</a>
+                <a href="{{ url('/image') }}" class="tag-cloud-link">Image</a>
+                <a href="{{ url('/voice') }}" class="tag-cloud-link">Voice</a>
+                <a href="{{ url('/word') }}" class="tag-cloud-link">Word</a>
+                <a href="{{ url('/test') }}" class="tag-cloud-link">Test</a>
+              </div>
+            </div>
+			</div>
+		</div>
+
+	
+
+	<div class="row justify-content-center mb-5">
+          <div class="col-md-7 heading-section text-center ftco-animate ">
+            <h2 class="mb-3">Key Words</h2>
+			<h5>1. Venetian Roman 威尼斯羅馬體</h5>
+            <p>特色：e 的橫桿會往右上飛起甚至突出，大寫字母 O、C、G 軸心左傾。造型最具手寫特徵。</p>
+			<p>代表字型：Jenson（它的驚嘆號、逗號不是圓形是菱形，保留平頭筆精神）、Centaur（日本愛用，有古典感）、Italian Old Style。</p>
+			<h5>2. Old Roman 古典羅馬體</h5>
+			<p>特色：雖然大寫字母軸心一樣是左傾的，但 e 的橫桿是水平的。常用於文藝場合。</p>
+			<p>代表字型：Garamond（誠品愛用字型，它的 p、9、6 都沒有連起來，相當好認）、Bembo（日本超常用）、Caslon（Q 比較厚重）</p>
+			<h5>3. Transitional Roman 過渡時期羅馬體</h5>
+			<p>特色：大寫字母軸心是垂直的，或僅有微微傾斜。理性學術派風格。</p>
+			<p>代表字型：Baskerville（工整大方）、Fourier（Q 往上勾）、Bulmer、Times（x-height 比較大，容易看膩(?)）</p>
+			<h5>4. Modem Roman 現代羅馬體</h5>
+			<p>特色：筆劃粗細對比極端強烈。時尚優雅。</p>
+			<p>代表字型：Didot、Bodoni、Walbaum（數字優美）</p>
+			<h5>5. Slab Serif 粗襯線體</h5>
+			<p>特色：筆畫粗細均一，連襯線都跟筆畫差不多粗。常用於報章標題或運動場合。</p>
+			<p>代表字型：Clarendon、Rockwell（大學棒球帽上常見）</p>
+			<h5>6. Sans Serif 無襯線體</h5>
+			<p>特色：沒有襯線。所有筆畫都是均一粗度。具現代感，適合科技產品。</p>
+			<p>代表字型：Futura（幾何感，j 只有一條線沒有勾起來）、Avant Garde （八零年代霓虹風格，有特別的連體）、Helvetica（瑞士風格）、Frutiger（機場常見，具良好易辨性）、Myriad（過去蘋果官方字型）、Gill Sans（文藝風格無襯線體，作者 Eric Gill 是個蠻有爭議的雕刻師。全家的 LOGO 就是這個字型）、Optima（臺北捷運站名字型）</p>
+			<h5>7. Wood type 木活字</h5>
+			<p>代表字型：Birch（太空戰士標題）、Blackoak、Mesquite。</p>
+			<h5>8. Script 手寫字型</h5>
+			<p>代表字型：Snell Roundhand</p>
+			<h5>9. Blackletter 歌德體</h5>
+			<p>代表字型：San Marco</p>
+          </div>
+		 </div>
+
+
+    <footer class="ftco-footer ftco-section img"> 	
+	<div class="row justify-content-center mb-5">
+        <div class="col-md-7 heading-section text-center ftco-animate ">
+          <h2 class="mb-3">Word Falls</h2>
+        </div>
+	</div>
+
+	<div class="row justify-content-center mb-5">
+		<!-- game -->
+		<iframe src="./WebGL/index.html" name="Word Falls" scrolling="no" frameborder="0" marginheight="px" marginwidth="1280px" height="720px" width="1280px"></iframe>
+	</div>
+    </footer>
+  
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
